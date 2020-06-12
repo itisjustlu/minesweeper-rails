49c61c79e29a7b673f2339f0b3da6be9dc1da84f
Lucas <lucasjavierjuarez@hotmail.com>
2020-06-12 00:27:39 -0300

    Adjacents

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -13,6 +13,7 @@ class Cell < ApplicationRecord
   def click!
     return unless can_click?
     update(clicked: true)
+    update_adjacents!(adjacents) if mines_around.to_i.zero? && board.playing?
     check_game_status!
   end
 
@@ -39,5 +40,20 @@ class Cell < ApplicationRecord
     none_cells = board.cells.where(type: "Cells::None")
     remaining = none_cells.count - none_cells.where(clicked: true).count
     board.win! if remaining.zero?
+  end
+
+  def adjacents
+    @adjacents ||= board.cells.where(row: row + 1, column: column)
+      .or(board.cells.where(row: row + 1, column: column + 1))
+      .or(board.cells.where(row: row, column: column + 1))
+      .or(board.cells.where(row: row - 1, column: column + 1))
+      .or(board.cells.where(row: row - 1, column: column))
+      .or(board.cells.where(row: row - 1, column: column - 1))
+      .or(board.cells.where(row: row, column: column - 1))
+      .or(board.cells.where(row: row + 1, column: column - 1))
+  end
+
+  def update_adjacents!(cells)
+    cells.update_all(clicked: true)
   end
 end
